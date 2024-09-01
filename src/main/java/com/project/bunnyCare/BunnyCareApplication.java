7c45a39745510e544ac9e2058c8b149a6252b05d
YeonJun7 <yeonjunapex.gmail.com>
2024-09-01 21:34:37 +0900

    Initializer Spring boot Project

---
+++ src/main/java/com/project/bunnyCare/BunnyCareApplication.java
@@ -0,0 +1,13 @@
+package com.project.bunnyCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunnyCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunnyCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bunnyCare/BunnyCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bunnyCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunnyCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
