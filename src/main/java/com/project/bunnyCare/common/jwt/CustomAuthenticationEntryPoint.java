813de69534b8da8c7c15d4e0df97fd4d9b6fbe0c
YeonJun7 <yeonjunapex.gmail.com>
2024-09-08 20:49:27 +0900

    Fix jwt 예외 발생시 401 에로 코드응답으로 수정

--- src/main/java/com/project/bunnyCare/common/jwt/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/project/bunnyCare/common/jwt/CustomAuthenticationEntryPoint.java
@@ -27,7 +27,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         }
         log.error("AuthenticationEntryPoint: {}", code.getMessage());
         response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(code.getCode());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         ObjectMapper obj = new ObjectMapper();
         ApiResponse<Void> api = ApiResponse.exception(code);
         response.getWriter().println(obj.writeValueAsString(api));
